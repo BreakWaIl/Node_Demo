b331aa8b226c00902d3b85488b435675c7ba89e7
BreakWaIl <ly0692@vip.qq.com>
2019-03-19 22:36:58 +0800

    fix

--- demo.js
+++ demo.js
@@ -2,26 +2,36 @@
 let  _ =  require('lodash');
 
 let obj = {
-    "s":{
-        "red1":[],
-        "blue1":[]
+    "fq":{
+        "r":[],
+        "b":[]
     },
-    "d":{
-        "red2":[],
-        "blue2":[]
+    "tt":{
+        "r":[],
+        "b":[]
     }
 };
 
-for(let i = 1;i<=1000000;i++){
-    let red1 = _.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33],6);
-    let blue1 = _.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],1);
-    let red2 = _.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],5);
-    let blue2 = _.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12],2);
-    obj["s"]["red1"] = _.sortBy(red1);
-    obj["s"]["blue1"] = _.sortBy(blue1);
-    obj["d"]["red2"] = _.sortBy(red2);
-    obj["d"]["blue2"] = _.sortBy(blue2);
+let [tqR,tqB,ttR,ttB] = [[],[],[],[]];
 
+for(let i = 1;i<=1000000;i++){
+    tqR.push(_.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33],6));
+    tqB.push(_.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],1));
+    ttR.push(_.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],5));
+    ttB.push(_.sampleSize([1,2,3,4,5,6,7,8,9,10,11,12],2));
 }
 
+let [qR,qB,tR,tB] = [
+    _.sortBy(tqR[_.random(tqR.length-1,1)]),
+    _.sortBy(tqB[_.random(tqB.length-1,1)]),
+    _.sortBy(ttR[_.random(ttR.length-1,1)]),
+    _.sortBy(ttB[_.random(ttB.length-1,1)])
+];
+
+obj["fq"]["r"] = qR;
+obj["fq"]["b"] = qB;
+obj["tt"]["r"] = tR;
+obj["tt"]["b"] = tB;
+
 console.log(obj);
+
